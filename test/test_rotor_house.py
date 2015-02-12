68a76b0a93ae462d6d464ad76dfd9957b642e396
Nathan Wilson <nordaq@gmail.com>
2015-02-12 23:26:15 +0000

    Clean up test_rotor_house to add more readability

--- test/test_rotor_house.py
+++ test/test_rotor_house.py
@@ -6,15 +6,13 @@ from src.exceptions.exceptions import NoRotorsInHouseException
 class TestRotorHouse(unittest.TestCase):
     def setUp(self):
         self.rotor_house = RotorHouse()
-
-    def test_rotor_house_is_created(self):
-        self.assertIsNotNone(self.rotor_house)
     
     def test_house_with_one_rotor_maps_given_input_to_expected_output(self):
         input_character = 'A'
         expected_output_character = 'E'
         self.rotor_house.add_rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ')
-        self.assertEqual(self.rotor_house.process_letter(input_character),
+        actual_output_character = self.rotor_house.process_letter(input_character)
+        self.assertEqual(actual_output_character,
                 expected_output_character,
                 "Expected the output to be 'E' when given input 'A'")
 
@@ -23,7 +21,8 @@ class TestRotorHouse(unittest.TestCase):
         expected_output_character = 'S'
         self.rotor_house.add_rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ')
         self.rotor_house.add_rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE')
-        self.assertEqual(self.rotor_house.process_letter(input_character),
+        actual_output_character = self.rotor_house.process_letter(input_character)
+        self.assertEqual(actual_output_character,
                 expected_output_character,
                 "Expected the output to be 'S' when given input 'A'")
 
