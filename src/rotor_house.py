d60cfc325fd063ff6735a1967d47624f4754895d
Nathan Wilson <nordaq@gmail.com>
2015-02-12 23:10:22 +0000

    Update __rotor variable in RotorHouse to be a list to accept more than one Rotor

--- src/rotor_house.py
+++ src/rotor_house.py
@@ -4,13 +4,14 @@ from exceptions.exceptions import NoRotorsInHouseException
 
 class RotorHouse:
    
-    __rotor = None
+    __rotor = []
 
     def add_rotor(self, rotor_substitution_order): 
-        self.__rotor = Rotor(rotor_substitution_order)
+        self.__rotor.append(Rotor(rotor_substitution_order))
+        print self.__rotor
 
     def process_letter(self, input_character):
-        if self.__rotor is None:
+        if not self.__rotor:
             raise(NoRotorsInHouseException)
         else:
-            return self.__rotor.get_substitution_character_for_given_input(input_character)
+            return self.__rotor[0].get_substitution_character_for_given_input(input_character)
