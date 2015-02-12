1ba6b799082addd99684ffe043188d4248ab62e1
Nathan Wilson <nordaq@gmail.com>
2015-02-12 23:55:15 +0000

    Refactor checking that rotor_house has rotors into separate method which is called in place of old if statment. Can be used in new process_string() method

--- src/rotor_house.py
+++ src/rotor_house.py
@@ -12,17 +12,19 @@ class RotorHouse:
     def add_rotor(self, rotor_substitution_order): 
         self.__rotor.append(Rotor(rotor_substitution_order))
 
-    def process_letter(self, input_character):
-        if not self.__rotor:
+    def check_house_has_rotors(self):
+    	if not self.__rotor:
             raise(NoRotorsInHouseException)
-        else:
-        	current_character = input_character
-        	previous_rotor = None
-        	for rotor in self.__rotor:
-        		current_character = rotor.get_substitution_character_for_given_input(current_character)
-        		if previous_rotor is None:
-        			rotor.rotate()
-        		elif previous_rotor.get_current_rotation_value() is 0:
-        			rotor.rotate()
-        		previous_rotor = rotor
-        	return current_character
+
+    def process_letter(self, input_character):
+    	self.check_house_has_rotors()
+    	current_character = input_character
+    	previous_rotor = None
+    	for rotor in self.__rotor:
+    		current_character = rotor.get_substitution_character_for_given_input(current_character)
+    		if previous_rotor is None:
+    			rotor.rotate()
+    		elif previous_rotor.get_current_rotation_value() is 0:
+    			rotor.rotate()
+    		previous_rotor = rotor
+    	return current_character
