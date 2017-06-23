7902a8c20904acdd25a8e752ba7411761348cafa
Nathan Wilson <nordaq@gmail.com>
2017-06-23 23:01:28 +0100

    check input is all upper case else exit

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ from plugboard import Plugboard
 
 import sys
 
+input_string = str(sys.argv[1])
+if not input_string.isupper():
+    print "Input must be all upper case"
+    exit(1)
+
 plugboard = Plugboard('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 rotor_house = RotorHouse()
@@ -12,7 +17,7 @@ rotor_house.add_rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q')
 rotor_house.add_rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E')
 rotor_house.add_reflector('EJMZALYXVBWFCRQUONTSPIKHGD')
 
-input_string = str(sys.argv[1])
+
 print input_string
 
 print rotor_house.process_string(plugboard.process_string(input_string))
